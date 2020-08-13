b0f31f3cbd928d1990d6fd4b02ee7b97fef85ec1
dhruv <dhruvshah78223@gmail.com>
2020-08-13 21:30:21 +0530

    Initial Commit.

---
+++ src/app/SubjectMarks/add-subject-marks/add-subject-marks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-subject-marks',
+  templateUrl: './add-subject-marks.component.html',
+  styleUrls: ['./add-subject-marks.component.scss']
+})
+export class AddSubjectMarksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddSubjectMarksModule } from './SubjectMarks/add-subject-marks/add-subject-marks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AddSubjectMarksModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
