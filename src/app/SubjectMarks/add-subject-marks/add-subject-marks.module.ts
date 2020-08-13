41969a7bf681399fa99e8b214f26b68368c04881
dhruv <dhruvshah78223@gmail.com>
2020-08-13 21:30:21 +0530

    Initial Commit.

---
+++ src/app/SubjectMarks/add-subject-marks/add-subject-marks.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddSubjectMarksComponent } from './add-subject-marks.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddSubjectMarksComponent]
+})
+export class AddSubjectMarksModule { }
